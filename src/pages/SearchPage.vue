1d2cd9febc0c77db0fa580fc9b52c331c15d40d7
Azreil-OFD <fantom2413@gmail.com>
2024-12-01 04:20:41 +0500

    fix

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -261,13 +261,12 @@ function formatRequestData() {
         state: selectedStatus.value.length > 0 ? selectedStatus.value.map(item => item.code) : [selectedStatus.value],  // Состояния (массив кодов)
         page: 1,
     };
-    console.log(useWebApp().initDataUnsafe)
     // Отправка данных через curl с использованием fetch API
     fetch('https://vcc-bot.cloudpub.ru/api/vcc/meetings/', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'init-data': userInitialData.value || '', // Здесь передаем initialData пользователя
+            'init-data': JSON.stringify(useWebApp().initDataUnsafe) || '', // Здесь передаем initialData пользователя
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(requestData),
